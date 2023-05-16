66ed3f3b1616b716bf813f3ccda969439233899a
Tufan AKÇAY <132299647+tufanakcay@users.noreply.github.com>
2023-05-16 21:46:43 +0300

    update java

--- app/src/main/java/com/tufanakcay/javatravelbook/view/MainActivity.java
+++ app/src/main/java/com/tufanakcay/javatravelbook/view/MainActivity.java
@@ -1,16 +1,85 @@
 package com.tufanakcay.javatravelbook.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.room.Room;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.tufanakcay.javatravelbook.R;
+import com.tufanakcay.javatravelbook.adapter.PlaceAdapter;
+import com.tufanakcay.javatravelbook.databinding.ActivityMainBinding;
+import com.tufanakcay.javatravelbook.model.Place;
+import com.tufanakcay.javatravelbook.roomdb.PlaceDao;
+import com.tufanakcay.javatravelbook.roomdb.PlaceDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+    private final CompositeDisposable mDisposable = new CompositeDisposable();
+    ArrayList<Place> places;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        places = new ArrayList<>();
+
+        PlaceDatabase db = Room.databaseBuilder(getApplicationContext(),
+                PlaceDatabase.class, "Places").allowMainThreadQueries().build();
+
+        PlaceDao placeDao = db.placeDao();
+
+        mDisposable.add(placeDao.getAll()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::handleResponse));
+
+    }
+
+    private void handleResponse(List<Place> placeList) {
+
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        PlaceAdapter placeAdapter = new PlaceAdapter(placeList);
+        binding.recyclerView.setAdapter(placeAdapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.travel_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.add_place) {
+            Intent intent = new Intent(this,MapsActivity.class);
+            intent.putExtra("info","new");
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mDisposable.clear();
     }
 }
