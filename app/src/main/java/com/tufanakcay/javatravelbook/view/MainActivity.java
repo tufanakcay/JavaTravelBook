ebcf1f5d56b340bb03e77e0ed52760ff4c30e599
Tufan AKÇAY <132299647+tufanakcay@users.noreply.github.com>
2023-05-16 21:31:51 +0300

    Update

---
+++ app/src/main/java/com/tufanakcay/javatravelbook/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tufanakcay.javatravelbook.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tufanakcay.javatravelbook.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
